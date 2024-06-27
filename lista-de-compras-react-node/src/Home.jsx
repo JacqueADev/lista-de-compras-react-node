260119b4658ac441b8f4dc8dee70c101072c2aa5
JacqueADev <jacquelineadev@gmail.com>
2024-06-27 19:47:28 -0300

    Function onClick adicionada

--- lista-de-compras-react-node/src/Home.jsx
+++ lista-de-compras-react-node/src/Home.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function Home() {
- 
+ function cliqueiNoBotao(){
+  console.log("Cliquei")
+ }
   return (
     <div>
       <h1>Comprar:</h1>
       <input placeholder="Produto..." type="text" />
-      <button>Adicionar a lista</button>
+      <button onClick={cliqueiNoBotao} >Adicionar a lista</button>
     </div>
   )
 }
