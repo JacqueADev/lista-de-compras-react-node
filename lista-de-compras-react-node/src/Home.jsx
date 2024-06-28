1234905e524fe475f0f78e61939e6240343dfc09
JacqueADev <jacquelineadev@gmail.com>
2024-06-28 16:53:30 -0300

    Criação useState, UseRef, uuid (v4)

--- lista-de-compras-react-node/src/Home.jsx
+++ lista-de-compras-react-node/src/Home.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import { v4 } from "uuid";
 
 function Home() {
- function cliqueiNoBotao(){
-  console.log("Cliquei")
- }
+  const [produtos, setProdutos] = useState([]);
+  const inputRef = useRef();
+
+  function cliqueiNoBotao() {
+    console.log(v4());
+    setProdutos([{ id: v4(), nome: inputRef.current.value }, ...produtos]);
+  }
+
   return (
     <div>
       <h1>Comprar:</h1>
-      <input placeholder="Produto..." type="text" />
-      <button onClick={cliqueiNoBotao} >Adicionar a lista</button>
+      <input placeholder="Produto..." type="text" ref={inputRef} />
+      <button onClick={cliqueiNoBotao}>Adicionar a lista</button>
+
+      {produtos.map((produto) => (
+        <div key={produto.id}>{produto.nome}</div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
+//parei minuto 27:47 deste link: https://www.youtube.com/live/3wcjhAZoviA?si=0qFr1qaIfXwRewFH
